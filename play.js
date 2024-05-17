e26cf2c2d23033a87315ae2f073816ae7e303c01
Ann <a.m.hnatyshyn@gmail.com>
2024-05-16 21:38:07 -0300

    add connection function

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '192.168.4.20',
+    port: '50541'
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+  });
+  
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
