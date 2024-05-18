54857fe3940036fa1dabdabfc7681bf6380f2e59
Ann <a.m.hnatyshyn@gmail.com>
2024-05-18 19:54:59 -0300

    call setupInput(connection)

--- input.js
+++ input.js
@@ -1,7 +1,7 @@
 const connect = require("./client.js");
-let connection;
+let connection = connect();
 
-const setupInput = function(conn) {
+const setupInput = (conn) => {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -14,22 +14,18 @@ const handleUserInput = function(key) {
   if (key === "\u0003") {
     process.exit();
   }
-};
-const movement = function(key) {
   if (key === "w") {
-    console.log("Move: up");
+    connection.write("Move: up");
   }
   if (key === "a") {
-    console.log("Move: left");
+    connection.write("Move: left");
   }
   if (key === "s") {
-    console.log("Move: down");
+    connection.write("Move: down");
   }
   if (key === "d") {
-    console.log("Move: right");
+    connection.write("Move: right");
   }
 };
-
-connect();
-setupInput();
+setupInput(connection);
 module.exports = setupInput;
