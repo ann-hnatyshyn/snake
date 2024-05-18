55ee652e8681bec9091a7889414a3a24c8667e1d
Ann <a.m.hnatyshyn@gmail.com>
2024-05-18 19:04:36 -0300

    wasd movement & connection

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 const connect = require("./client.js");
+let connection;
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,14 +10,26 @@ const setupInput = function() {
   stdin.on("data", handleUserInput);
   return stdin;
 };
-
 const handleUserInput = function(key) {
   if (key === "\u0003") {
     process.exit();
   }
 };
+const movement = function(key) {
+  if (key === "w") {
+    console.log("Move: up");
+  }
+  if (key === "a") {
+    console.log("Move: left");
+  }
+  if (key === "s") {
+    console.log("Move: down");
+  }
+  if (key === "d") {
+    console.log("Move: right");
+  }
+};
 
 connect();
 setupInput();
-
 module.exports = setupInput;
