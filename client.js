b18a83e126436d78eced375c47d89aab3f61b14f
Ann <a.m.hnatyshyn@gmail.com>
2024-05-18 14:22:25 -0300

    fix name to server

--- client.js
+++ client.js
@@ -3,17 +3,13 @@ const net = require("net");
 const connect = function() {
   const conn = net.createConnection({
     host: "localhost",
-    port: "50541",
+    port: 50541,
   });
   conn.setEncoding("utf8");
 
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-  conn.on("Connect", () => {
-    console.log("Server says: Successfully connected to game server!", connect);
-    const playerName = "Name: AMH";
-    conn.write(playerName);
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: AMH");
   });
 
   return conn;
